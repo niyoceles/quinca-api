a783e8b13ab628537c518b164b1cdb6f51c5a7a5
Celestin NIYONSABA <niyoceles3@gmail.com>
2021-02-10 13:09:13 +0200

    add allow methods

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
