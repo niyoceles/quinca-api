e9584413e31f5e31affdbf2a62b745302cce5399
Celestin NIYONSABA <niyoceles3@gmail.com>
2021-02-10 13:01:45 +0200

    add all cors

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.use(
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/api', router);
 
 // Error handling to catch 404
